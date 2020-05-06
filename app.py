d591412bc12ca8f933ba3e138f21c48fd082d10a
leisyarcia <leisyarcia@gmail.com>
2020-05-06 21:41:04 +0000

    Finished

--- app.py
+++ app.py
@@ -1,9 +1,10 @@
 import random
-suites = ['♡', '♢', '♤', '♧']
-values = list(range(1,14))
+suites = ["♡","♧","♤","♢"]
+values = ["ace","king","queen","jacks","10","9","8","7","6","5","4","3","2"]
 
-def get_random_card():
-    # your code here
-    return "3♤"
-    
-print(get_random_card())
+random_suites_position=random.randint(0,3)
+random_values_position=random.randint(0,12)
+
+generate_card=suites[random_suites_position]+values[random_values_position]
+
+print(generate_card)
